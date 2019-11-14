9c525cf810d2b8e85b291172b982a877634ac913
Georgii S <42458018+Emtyset@users.noreply.github.com>
2019-11-15 02:25:51 +0500

    Update ravdess.py
    * Загрузчик упрощен
    * Добавлен работающий класс-аугментатор для MFCC

--- ravdess.py
+++ ravdess.py
@@ -1,92 +1,109 @@
 import librosa
 import librosa.display
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 import os
 import shutil
 import math
 import joblib
-
+from keras.utils import to_categorical
 from IPython.display import clear_output, display
 
-def progress(idx, detail=''):
+
+almost_every_augmentation = [
+    ('shift_by_second', {'shift_max': 2}), 
+    ('noise_injection', {'noise_factor' : 0.4}),
+    ('pitch_change', {'pitch_max' : 5, 'pitch_min' : -5})
+]
+
+
+def progress(idx, detail=None):
     clear_output(wait=True)
-    display(detail + "Done: " + str(idx + 1))
+    if detail is None:
+        display("Done: " + str(idx + 1))
+    else:
+        display(detail + " done: " + str(idx + 1))
 
     
+def create_all_in_one_folder(raw_data_path, fname):
+    RAW_DATA_PATH = raw_data_path
+    ALL_IN_ONE_PATH = os.path.join(RAW_DATA_PATH, fname)
+
+    if not os.path.exists(ALL_IN_ONE_PATH):
+        os.mkdir(os.path.abspath(ALL_IN_ONE_PATH))
+
+    for i in range(1, 25):
+        actor_path = RAW_DATA_PATH
+        if i < 10:
+            actor_path = RAW_DATA_PATH + '/Actor_0' + str(i)
+        else:
+            actor_path = RAW_DATA_PATH + '/Actor_' + str(i)
+        for file in os.listdir(os.path.abspath(actor_path)):
+            shutil.copy(actor_path + '/' + str(file), os.path.abspath(ALL_IN_ONE_PATH))
+    
+    return ALL_IN_ONE_PATH
+
+
 class Loader:
-    def __init__(self, path=None, start=0, amount=None, all_in_one_folder=False, all_in_one_folder_name='All'):
+    def __init__(self, path=None, amount=None, shuffle=False):
         self.path = path
-        self.start = start
-        self.end = amount + start if amount is not None else None
         self.data = None
-        if path is not None:
-            if not all_in_one_folder:
-                self.create_all_in_one_folder(path, all_in_one_folder_name)
-                self.path = path + all_in_one_folder_name + '/'
-            self.filelist = np.array(os.listdir(os.path.abspath(self.path)))[self.start:self.end]
-            self.labels = self.load_labels()
-        self.decryptor = {
-            0 : 'neutral', 
-            1 : 'calm', 
-            2 : 'happy', 
-            3 : 'sad', 
-            4 : 'angry', 
-            5 : 'fearful', 
-            6 : 'disgust', 
-            7 : 'surprised'
-        }
+        self.labels = None
+        if self.path is not None:
+            self.path = path if path[-1] == '/' else path + '/'
+            self.filelist = np.array(os.listdir(os.path.abspath(self.path)))
+            if shuffle:
+                idxs = np.random.permutation(len(self.filelist))
+                self.filelist = self.filelist[idxs]
+            self.filelist = self.filelist[:amount]
+            
         
-                
-
-    def create_all_in_one_folder(self, raw_data_path, fname):
-        RAW_DATA_PATH = raw_data_path
-        ALL_IN_ONE_PATH = os.path.join(RAW_DATA_PATH, fname)
-
-        if not os.path.exists(ALL_IN_ONE_PATH):
-            os.mkdir(os.path.abspath(ALL_IN_ONE_PATH))
-
-        for i in range(1, 25):
-            actor_path = RAW_DATA_PATH
-            if i < 10:
-                actor_path = RAW_DATA_PATH + '/Actor_0' + str(i)
-            else:
-                actor_path = RAW_DATA_PATH + '/Actor_' + str(i)
-            for file in os.listdir(os.path.abspath(actor_path)):
-                shutil.copy(actor_path + '/' + str(file), os.path.abspath(ALL_IN_ONE_PATH))
-
-        return ALL_IN_ONE_PATH
-    
-    def load_labels(self):
+    def load_labels(self, to1hot=False):
         labels = []
         for file in self.filelist:
-            labels.append([int(file[6:8])])
-        return np.array(labels)
+            labels.append([int(file[6:8]) - 1])
         
-    def load_instances_list(self): 
+        self.labels = np.array(labels)
+        if to1hot:
+            self.labels = to_categorical(labels,num_classes=8)
+        return self.labels
+    
+    def load_data(self, sr=22050):
         if self.path is None:
-            raise NameError("No path")
-        data_list = []
+            raise NameError("No Path")
 
+        data_list = []
         for idx, file in enumerate(self.filelist):
             x, s = librosa.core.load(self.path + file, res_type='kaiser_fast')
             data_list.append(x)
-            progress(idx)
-            
+            progress(idx, 'Loading')
         self.data = data = np.array(data_list)
         print('Load complete')
-        return self.data, self.labels
+        return self.data
+    
+    def load_dataset(self, sr=22050, to1hot=False):
+        return self.load_data(sr=sr), self.load_labels(to1hot=to1hot)
 
-    def save_via_joblib(self, external_data=None, save_dir='./', names=('X', 'y')):
-        if self.path is None:
-            raise NameError("No path")
-        if self.data is None:
-            raise NameError("Didn't load")
-        if external_data is not None:
-            self.data = external_data
+    def save_via_joblib(self, external_data=(None, None), save_dir='./', names=('X', 'y')):
+        
+
+            
+        if external_data[0] is not None:
+            self.data = external_data[0]
+            
+        if external_data[1] is not None:
+            self.labels = external_data[1]
+        
+        if self.data is None :
+            raise NameError("Didn't load data")
+            
+        if self.labels is None:
+            raise NameError("Didn't load labels")
+            
         if not os.path.exists(save_dir):
             os.mkdir(os.path.abspath(save_dir))
+            
+        
+            
         joblib.dump(self.data, os.path.join(save_dir, names[0]))
         joblib.dump(self.labels, os.path.join(save_dir, names[1]))
     
@@ -97,95 +114,97 @@ class Loader:
         self.labels = joblib.load(pathY)
         return self.data, self.labels
 
-def get_mfcc(data, sampling_rate, n_mfcc=40):
+    
+def get_mfcc(data, sampling_rate, n_mfcc=40, conv_shaping=False):
     mfcc_array = []
     for idx, inst in enumerate(data):
         mfcc = np.mean(librosa.feature.mfcc(y=inst, sr=sampling_rate, n_mfcc=n_mfcc).T, axis=0)
         mfcc_array.append(mfcc)
         progress(idx)
+    if conv_shaping:
+        return np.reshape(np.array(mfcc_array), (len(data), n_mfcc, 1))
     return np.array(mfcc_array)
 
-def noise_injection(data, noise_factor):
-    augmented_data = list()
-    for idx, inst in enumerate(data):
-        noise = np.random.randn(len(inst))
-        augmented_inst = inst + noise_factor * noise / 100
-        augmented_inst = inst.astype(np.float32)
-        augmented_data.append(augmented_inst)
-        progress(idx)
-    return np.array(augmented_data)
 
-def shift_by_proportion(data, shift_max, shift_direction='both'):
-    augmented_data = list()
-    for idx, inst in enumerate(data):
-        shift = np.random.randint(int(shift_max * len(inst)))
-        if shift_direction == 'left':
-            shift = -shift
-        elif shift_direction == 'both':
-            direction = np.random.randint(0, 2)
-            if direction == 1:
-                shift = -shift    
-        augmented_inst = np.roll(inst, shift)
-        if shift > 0:
-            augmented_inst[:shift] = 0
-        else:
-            augmented_inst[shift:] = 0
-        augmented_data.append(augmented_inst)
-        progress(idx)
-    return np.array(augmented_data)
+class MFCC_Prep:
+    def __init__(self, sampling_rate=22050, n_mfcc=40):
+        self.sampling_rate = sampling_rate
+        self.n_mfcc = n_mfcc
 
-def shift_by_second(data, shift_max, sampling_rate, shift_direction='both'):
-    augmented_data = list()
-    for idx, inst in enumerate(data):
-        shift = np.random.randint(sampling_rate * shift_max)
-        if shift_direction == 'left':
-            shift = -shift
-        elif shift_direction == 'both':
-            direction = np.random.randint(0, 2)
-            if direction == 1:
-                shift = -shift    
-        augmented_inst = np.roll(inst, shift)
-        if shift > 0:
-            augmented_inst[:shift] = 0
-        else:
-            augmented_inst[shift:] = 0
-        augmented_data.append(augmented_inst)
-        progress(idx)
-    return np.array(augmented_data)
+            
+    def augment_separately(self, data, labels, augmentations=[]):
+        
+        def get_mfcc(data):
+            return np.mean(librosa.feature.mfcc(y=data, sr=self.sampling_rate, n_mfcc=self.n_mfcc).T,axis=0)
 
-def pitch_change(data, sampling_rate, pitch_factor):
-    augmented_data = list()
-    for idx, inst in enumerate(data):
-        augmented_data.append(librosa.effects.pitch_shift(inst, sampling_rate, pitch_factor))
-        progress(idx)
-    return np.array(augmented_data)
+        def noise_injection(data, noise_factor):
+            noise = np.random.randn(len(data))
+            augmented_data = data + noise_factor * noise / 100
+            augmented_data = augmented_data.astype(type(data[0]))
+            return augmented_data
 
-def trim(data):
-    augmented_data = list()
-    for idx, inst in enumerate(data):
-        augmented_data.append(librosa.effects.trim(inst)[0])
-        progress(idx)
-    return np.array(augmented_data)
+        def shift_by_proportion(data, shift_max, shift_direction='both'):
 
-def stretch_to_size(data, sampling_rate, expected_duration='max'):
-    if expected_duration == 'max':
-        expected_len = max([len(inst) for inst in data])
-    else:
-        expected_len = expected_duration * sampling_rate
-    augmented_data = list()
-    for idx, inst in enumerate(data):
-        rate = len(inst) / expected_len
-        augmented_data.append(librosa.effects.time_stretch(inst, rate))
-        progress(idx)
-    return np.array(augmented_data)
+            shift = np.random.randint(int(shift_max * data.shape[-1]))
+            if shift_direction == 'right':
+                shift = -shift
+            elif shift_direction == 'both':
+                direction = np.random.randint(0, 2)
+                if direction == 1:
+                    shift = -shift    
+            augmented_data = np.roll(data, shift)
+            if shift > 0:
+                augmented_data[:shift] = 0
+            else:
+                augmented_data[shift:] = 0
+            return augmented_data
 
-def pad_to_size(data, sampling_rate, expected_duration='max'):
-    augmented_data = list()
-    if expected_duration == 'max':
-        expected_len = max([len(inst) for inst in data])
-    else:
-        expected_len = sampling_rate * expected_duration
-    for idx, inst in enumerate(data):
-        augmented_data.append(librosa.util.fix_length(inst, expected_len))
-        progress(idx)
-    return np.array(augmented_data)
+
+        def shift_by_second(data, shift_max, sampling_rate, shift_direction='both'):
+            shift = np.random.randint(sampling_rate * shift_max)
+            if shift_direction == 'right':
+                shift = -shift
+            elif shift_direction == 'both':
+                direction = np.random.randint(0, 2)
+                if direction == 1:
+                    shift = -shift    
+
+            augmented_data = np.roll(data, shift)
+            if shift > 0:
+                augmented_data[:shift] = 0
+            else:
+                augmented_data[shift:] = 0
+            return augmented_data
+
+        def pitch_change(data, pitch_max, pitch_min = 0):
+            pitch_factor = np.random.randint(low=pitch_min, high=pitch_max)
+            return librosa.effects.pitch_shift(data, self.sampling_rate, pitch_factor)
+
+        '''
+        def trim(data):
+            return librosa.effects.trim(data)
+        '''
+
+        def stretch_to_size(data, expected_duration):
+            rate = len(data) / (expected_duration * self.sampling_rate)
+            return librosa.effects.time_stretch(data, rate)
+        
+        augmented_data = get_mfcc(data[0])
+        augmented_labels = np.copy(labels)
+        
+        for idx, inst in enumerate(data[1:]):
+            augmented_data = np.vstack((augmented_data, get_mfcc(inst)))
+            progress(idx+1, 'copying')
+        
+        for (f, args) in augmentations:
+            func = locals()[f]
+            
+            for idx, inst in enumerate(data):
+                try:
+                    augmented_inst = func(data=inst, **args)
+                except TypeError:
+                    augmented_inst = func(data=inst, sampling_rate=self.sampling_rate, **args)
+                augmented_data = np.vstack((augmented_data, get_mfcc(augmented_inst)))
+                progress(idx, func.__name__)
+            augmented_labels = np.concatenate((augmented_labels, labels))
+        return augmented_data, augmented_labels
